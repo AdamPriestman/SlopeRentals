1f9f6c0861b0a329017c41870de9e0b256b8283b
Pooja Gurung <pooja_grg07@hotmail.com>
2023-11-21 15:21:14 +0000

    as per kata review, listings changed to listing on line 34 of listings_controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -31,7 +31,7 @@ class ListingsController < ApplicationController
   def update
     @listing = Listing.find(params[:id])
     @listing.update(listing_params)
-    redirect_to listings_path(@listing)
+    redirect_to listing_path(@listing)
   end
 
   private
