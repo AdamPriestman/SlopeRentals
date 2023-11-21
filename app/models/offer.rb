fec028b32f02e4fbabf293c4ea6d39b57cecedc4
Kata Nagy <nagykata98@gmail.com>
2023-11-21 17:14:44 +0000

    added start date validation to offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,4 +3,13 @@ class Offer < ApplicationRecord
   belongs_to :user
   validates :start_date, :end_date, presence: true
   validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
+  validate :start_date_in_future
+
+  private
+
+  def start_date_in_future
+    if start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
 end
