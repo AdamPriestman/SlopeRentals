36e9bbb681bd2fd63f49d73da7e33e727ca81efb
Adam Priestman <adampriestman@gmail.com>
2023-11-23 12:42:53 +0000

    changed root, styled home and index page, added search functionality

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,10 @@ class Listing < ApplicationRecord
   has_many :offers
   has_one_attached :photo
   validates :name, :price_per_day, :equipment_type, presence: true
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
