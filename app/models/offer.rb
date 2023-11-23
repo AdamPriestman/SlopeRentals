91742b8d38a67e998466105321d23a26a4eb31eb
Kata Nagy <nagykata98@gmail.com>
2023-11-23 16:05:28 +0000

    calendar update

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,7 +3,7 @@ class Offer < ApplicationRecord
   belongs_to :user
   validates :start_date, :end_date, presence: true
   validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
-  validate :start_date_in_future
+  validate :start_date_in_future, unless: -> { start_date.nil? }
 
   private
 
